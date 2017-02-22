1e9ffaf499bee84478212074bbe927ba56e504e3
reluo <21404683@qq.com>
2017-02-22 17:20:09 +0800

    route

--- src/router.js
+++ src/router.js
@@ -1,80 +1,84 @@
 import React from 'react'
-import {Router} from 'dva/router'
+import {Router,Route} from 'dva/router'
 import App from './routes/app'
-
+import PersonManage from './routes/BGManager/personManager'
 export default function ({history, app}) {
-  const routes = [
-    {
-      path: '/',
-      component: App,
-      getIndexRoute (nextState, cb) {
-        require.ensure([], require => {
-          cb(null, {component: require('./routes/welcome')})
-        })
-      },
-      childRoutes: [
-        {
-          path: 'dashboard',
-          name: 'dashboard',
-          getComponent (nextState, cb) {
-            require.ensure([], require => {
-              cb(null, require('./routes/dashboard'))
-            })
-          }
-        },
-        {
-          path: 'welcome',
-          name: 'welcome',
-          getComponent (nextState, cb) {
-            require.ensure([], require => {
-              cb(null, require('./routes/welcome'))
-            })
-          }
-        },
-        {
-          path: 'users',
-          name: 'users',
-          getComponent (nextState, cb) {
-            require.ensure([], require => {
-              cb(null, require('./routes/users'))
-            })
-          }
-        }, {
-          path: 'ui/ico',
-          name: 'ui/ico',
-          getComponent (nextState, cb) {
-            require.ensure([], require => {
-              cb(null, require('./routes/ui/ico'))
-            })
-          }
-        }, {
-          path: 'ui/search',
-          name: 'ui/search',
-          getComponent (nextState, cb) {
-            require.ensure([], require => {
-              cb(null, require('./routes/ui/search'))
-            })
-          }
-        },{
-          path: 'BGManager/personManager',
-          name: 'BGManager/personManager',
-          getComponent (nextState, cb) {
-            require.ensure([], require => {
-              cb(null, require('./routes/BGManager/personManager'))
-            })
-          }
-        }, {
-          path: '*',
-          name: 'error',
-          getComponent (nextState, cb) {
-            require.ensure([], require => {
-              cb(null, require('./routes/error'))
-            })
-          }
-        }
-      ]
-    }
-  ]
+  // const routes = [
+  //   {
+  //     path: '/',
+  //     component: App,
+  //     getIndexRoute (nextState, cb) {
+  //       require.ensure([], require => {
+  //         cb(null, {component: require('./routes/welcome')})
+  //       })
+  //     },
+  //     childRoutes: [
+  //       {
+  //         path: 'dashboard',
+  //         name: 'dashboard',
+  //         getComponent (nextState, cb) {
+  //           require.ensure([], require => {
+  //             cb(null, require('./routes/dashboard'))
+  //           })
+  //         }
+  //       },
+  //       {
+  //         path: 'welcome',
+  //         name: 'welcome',
+  //         getComponent (nextState, cb) {
+  //           require.ensure([], require => {
+  //             cb(null, require('./routes/welcome'))
+  //           })
+  //         }
+  //       },
+  //       {
+  //         path: 'users',
+  //         name: 'users',
+  //         getComponent (nextState, cb) {
+  //           require.ensure([], require => {
+  //             cb(null, require('./routes/users'))
+  //           })
+  //         }
+  //       }, {
+  //         path: 'ui/ico',
+  //         name: 'ui/ico',
+  //         getComponent (nextState, cb) {
+  //           require.ensure([], require => {
+  //             cb(null, require('./routes/ui/ico'))
+  //           })
+  //         }
+  //       }, {
+  //         path: 'ui/search',
+  //         name: 'ui/search',
+  //         getComponent (nextState, cb) {
+  //           require.ensure([], require => {
+  //             cb(null, require('./routes/ui/search'))
+  //           })
+  //         }
+  //       },{
+  //         path: 'BGManager/personManager',
+  //         name: 'BGManager/personManager',
+  //         getComponent (nextState, cb) {
+  //           require.ensure([], require => {
+  //             cb(null, require('./routes/BGManager/personManager'))
+  //           })
+  //         }
+  //       }, {
+  //         path: '*',
+  //         name: 'error',
+  //         getComponent (nextState, cb) {
+  //           require.ensure([], require => {
+  //             cb(null, require('./routes/error'))
+  //           })
+  //         }
+  //       }
+  //     ]
+  //   }
+  // ]
+const routes =
+  <Route path="/" component={App}>
+    <Route path="/BGManager/personManager" component={PersonManage}/>
+  </Route>
 
   return <Router history={history} routes={routes} />
 }
