5cbe1a770191180e1a5c895784fef5afcdbb666a
reluo <21404683@qq.com>
2017-04-25 21:17:30 +0800

    推广统计函数

--- src/services/Statistics/promoterStatistics.js
+++ src/services/Statistics/promoterStatistics.js
@@ -5,3 +5,58 @@ import AV from 'leancloud-storage'
 export async function getPromoterStatistics(payload){
   let statistics = await AV.Cloud.run('statPromoterPerformance',payload.date)
 }
+
+export async function fetchDaliyPerformance(payload) {
+  try{
+    let data = await AV.Cloud.run('fetchDaliyPerformance',payload)
+    return {success:true,data:data}
+
+  }catch (err){
+    return{success:false}
+  }
+}
+export async function fetchLastDaysPerformance(payload) {
+  try{
+    let data = await AV.Cloud.run('fetchLastDaysPerformance',payload)
+    return {success:true,data:data}
+
+  }catch (err){
+    return{success:false}
+  }
+}
+export async function fetchMonthPerformance(payload) {
+  try{
+    let data = await AV.Cloud.run('fetchMonthPerformance',payload)
+    return {success:true,data:data}
+
+  }catch (err){
+    return{success:false}
+  }
+}
+export async function fetchLastMonthsPerformance(payload) {
+  try{
+    let data = await AV.Cloud.run('fetchLastMonthsPerformance',payload)
+    return {success:true,data:data}
+
+  }catch (err){
+    return{success:false}
+  }
+}
+export async function fetchAreaMonthPerformance(payload) {
+  try{
+    let data = await AV.Cloud.run('fetchAreaMonthPerformance',payload)
+    return {success:true,data:data}
+
+  }catch (err){
+    return{success:false}
+  }
+}
+export async function fetchArealastMonthsPerformance(payload) {
+  try{
+    let data = await AV.Cloud.run('fetchArealastMonthsPerformance',payload)
+    return {success:true,data:data}
+
+  }catch (err){
+    return{success:false}
+  }
+}
